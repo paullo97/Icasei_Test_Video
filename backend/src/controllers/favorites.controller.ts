7be54f9fac454b1c3d90521edbff80b70b24a1d2
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-06-28 01:30:21 -0300

    include logic to include, remove and rescue values of favorite list

--- backend/src/controllers/favorites.controller.ts
+++ backend/src/controllers/favorites.controller.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from 'express';
+import '../models/video.model';
 
-export const listFavorites = async (req: Request, res: Response) => {
-  // Lógica para listar vídeos marcados como favoritos
+export const listFavorites = async (req: Request, res: Response<ListFavoritesResponse>) => {
+  const session = req.session;
+
+  if (!session || !session.favorites) {
+    return res.status(404).json({ message: 'Nenhum vídeo favorito encontrado', total: 0, favorites: [] });
+  }
+
+  const favoritesResponse: ListFavoritesResponse = {
+    favorites: session.favorites,
+    total: session.favorites.length
+  };
+
+  res.json(favoritesResponse);
 };
--- backend/src/controllers/videos.controller.ts
+++ backend/src/controllers/videos.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
 
 export const searchVideos = async (req: Request, res: Response) => {
-  // Lógica para buscar vídeos na API do YouTube
   const searchTem = req.query.term as string;
 
   res.json({ message: `${searchTem}`});
@@ -12,5 +11,20 @@ export const playVideo = async (req: Request, res: Response) => {
 };
 
 export const toggleFavorite = async (req: Request, res: Response) => {
-  // Lógica para adicionar/remover um vídeo dos favoritos
+  const { videoId } = req.params;
+  const session = req.session;
+
+  if (!Array.isArray(session.favorites)) {
+    session.favorites = [];
+  }
+
+  const index = session.favorites.indexOf(videoId);
+
+  if (index === -1) {
+    session.favorites.push(videoId);
+    res.json({ message: 'Vídeo adicionado aos favoritos' });
+  } else {
+    session.favorites.splice(index, 1);
+    res.json({ message: 'Vídeo removido dos favoritos' });
+  }
 };
