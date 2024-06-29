0f3a4d9ba0d1b1f933f1d2978861f3af123f066e
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-06-29 09:14:50 -0300

    include comment to resolve after

--- backend/src/routes/videos.router.ts
+++ backend/src/routes/videos.router.ts
@@ -4,7 +4,7 @@ import * as videosController from '../controllers/videos.controller';
 const router = express.Router();
 
 router.get('/search', videosController.searchVideos);
-router.get('/play/:videoId', videosController.playVideo);
+router.get('/play/:videoId', videosController.playVideo); // Maybe is not request this to backend, verify in front end if possible
 router.post('/toggle-favorite/:videoId', videosController.toggleFavorite);
 
 export default router;
